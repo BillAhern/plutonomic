01a30af3c636a03ad68b67004d8e837bd09bb753
Bill Ahern <william.ahern@gmail.com>
2024-02-23 13:51:15 -0500

    reconfig css for grid layout, setup for bento ui

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,7 @@ function App() {
 			</div>
 			<h1>Plutonomic</h1>
 			<div className='card'></div>
-			<p>
-				plutonomic (adj.) "of or pertaining to the science or study of wealth or riches," 1853, from Greek ploutos "wealth" (see Pluto) + ending from economic. Fell from
-				currency 1870s, revived 1990s. Related: Plutonomy (1851); plutonomics (1991, a 19c. word for "the science of wealth and the natural laws governing its production
-				and distribution" was plutology); plutonomist (1869).
-			</p>
+			<p>plutonomic (adj.) "of or pertaining to the science or study of wealth or riches".</p>
 			<FileConvert />
 			<FilterUnique />
 		</div>
