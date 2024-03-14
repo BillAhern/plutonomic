2e79ecb219186756bea4b2c00dfe6c606aaccdf2
Bill Ahern <william.ahern@gmail.com>
2024-03-14 09:56:22 -0400

    grid updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import FilterUnique from './components/filterUnique.tsx';
 
 function App() {
 	return (
-		<div className='grid-container' id="grid">
-			<div className='header'>
+		<div className='grid-container'>
+			<div className='header grid-item'>
 				<div className='header-row'>
 					<img className='logo' src={logo} alt='plutonomic logo' />
 					<h1 className='title'>Plutonomic</h1>
@@ -16,11 +16,11 @@ function App() {
 				</div>
 				<p>plutonomic (adj.) "of or pertaining to the science or study of wealth or riches".</p>
 			</div>
-			<div className='list-unique'>
+			<div className='list-unique grid-item'>
 				<FilterUnique />
 			</div>
-			<div className='daily-spend'>Daily Spend</div>
-			<div className='weekly-spend'>Weekly Spend</div>
+			<div className='daily-spend grid-item'>Daily Spend</div>
+			<div className='weekly-spend grid-item'>Weekly Spend</div>
 		</div>
 	);
 }
